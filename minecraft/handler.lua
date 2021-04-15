6b113ac0b2f9fc8f5f49309fb6243ac2b38d3f46
Stephen Leyva <sleyva1297@gmail.com>
2021-04-15 15:11:15 -0700

    Parse TCP packets and filter out pings

--- minecraft/handler.lua
+++ minecraft/handler.lua
@@ -1,6 +1,7 @@
 local redis = require 'redis'
 local http = require "socket.http"
 local json = require 'JSON'
+local bit32 = require 'bit32'
 
 local params = {
     host = 'redis-master.default.svc.cluster.local',
@@ -9,6 +10,77 @@ local params = {
 
 local client = redis.connect(params)
 local kong = kong
+
+local Packet = {
+    peek_count = 0,
+    sock = {}
+}
+
+function Packet:new()
+    local sock = assert(ngx.req.socket())
+    local peek_count = 0
+    local packet = Packet
+    packet.peek_count = peek_count
+    packet.sock = sock
+    return packet
+end
+
+function Packet:read_varint()
+    local num_read = 0
+    local value = 0
+    while (true) do
+        local current = self:get_u8()
+        local result = bit32.band(current, 127)
+        value = bit32.bor(value, bit32.lshift(result, 7 * num_read))
+        num_read = num_read + 1;
+
+        if bit32.band(current, 128) == 0 then
+            break
+        end
+    end
+    return value
+end
+
+function Packet:get_u8()
+    self.peek_count = self.peek_count + 1
+    local bytes = assert(self.sock:peek(self.peek_count))
+    local data = string.byte(bytes:sub(self.peek_count))
+    kong.log.debug("Bytes for payload: ", data)
+    return data
+end
+
+function Packet:read_bytes_as_string(count)
+    local original_peek_count = self.peek_count
+    self.peek_count = self.peek_count + count
+    local bytes = assert(self.sock:peek(self.peek_count))
+    bytes = bytes:sub(original_peek_count + 1)
+    kong.log.debug("String for payload: ", bytes)
+    return bytes
+end
+
+function Packet:is_ping()
+    local length = self:read_varint()
+    kong.log.debug("packet len is: ", length)
+
+    local packet_id = self:read_varint()
+    kong.log.debug("packet id is: ", packet_id)
+
+    if packet_id == 0 then
+        local mc_version = self:read_varint()
+        kong.log.debug("minecraft version:", mc_version)
+        local name_size = self:read_varint()
+        kong.log.debug("hostname size:", mc_version)
+        local name = self:read_bytes_as_string(name_size + 2) --Size plus unsigned short
+        kong.log.debug("HostName: ", name)
+        local status = self:read_varint()
+        kong.log.debug("status: ", status)
+        if status == 1 then
+            kong.log.debug("Status Packet From Client")
+            return true
+        end
+    end
+    return false
+end
 
 local function get_mapping(port)
     local name = client:get(string.format("%d", port))
@@ -54,6 +126,7 @@ local function remove_connection(name)
     local count = client:decr(name)
     kong.log.debug(string.format("current connected: %d", count))
     if count <= 0 then
+        client:set(name, 0)
         kong.log(string.format("None connected: sending shutdown signal to control plane: %s", name))
         local headers = { 
             Authorization="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.MdtOSGktuwpjR8KcOkwbw0IkSPe1JuQadcZAhGie4m0"
@@ -63,7 +136,7 @@ local function remove_connection(name)
             headers=headers,
             method="PUT",
         }
-        assert(status == 200, "status not ok")
+        assert(status == 200 or status == 208, "status not ok")
     end
 end
 
@@ -87,6 +160,12 @@ function WorldScaler:init_worker()
 end
 
 function WorldScaler:preread(conf)
+    local packet = Packet.new()
+    if packet:is_ping() then
+        kong.log("Ping packet detected, not scaling")
+        return
+    end
+
     WorldScaler.super.preread(self)
     local port = kong.request.get_port()
     local name = get_mapping(port)
@@ -97,6 +176,11 @@ function WorldScaler:log(conf)
     WorldScaler.super.log(self)
     local port = kong.request.get_port()
     local name = get_mapping(port)
+    if tonumber(client:get(name)) <= 0 then
+        kong.log("No connections on host")
+        return
+    end
+
     remove_connection(name)
 end
 
